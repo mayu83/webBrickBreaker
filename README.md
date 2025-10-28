20c192875d04cf470f616b7f77b0276cadb00e91
MaYu <a27db95e-e280-4b34-82e7-0c03e11167d4-214644356866119755_dsdev@noreply.codeup.aliyun.com>
2025-10-28 15:29:39 +0800

    push code

---
+++ README.md
@@ -0,0 +1,9 @@
+# BrickBreaker
+This is a work demonstrating the use of PIXI.js.
+This project utilized these existing codes:
+- PIXI.js - https://github.com/pixijs/pixijs
+- bump.js  - https://github.com/kittykatattack/bump
+- sound.js - https://github.com/kittykatattack/sound.js
+
+# Project Introduction
+After the game starts, you can first adjust the launch angle of the ball by using the left and right arrow keys on the keyboard. Press the spacebar to launch the ball. Use the left and right arrow keys to control the slider and catch the small ball.
---
+++ js/keyboard.js
@@ -0,0 +1,49 @@
+function keyboard(value, loop){
+    let key = {};
+    key.value = value;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    key.loop = false;
+    if(loop){
+        key.loop = true;
+    }
+    
+    key.downHandler = event => {
+        if (event.key === key.value) {
+            if ((key.loop || key.isUp) && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+            event.preventDefault();
+        }
+    }
+
+    key.upHandler = event => {
+        if (event.key === key.value) {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+            event.preventDefault();
+        }
+    }
+
+    const downListener = key.downHandler.bind(key);
+    const upListener = key.upHandler.bind(key);
+
+    window.addEventListener(
+        'keydown', downListener, false
+    );
+
+    window.addEventListener(
+        'keyup', upListener, false
+    );
+
+    key.unsubscribe = () => {
+        window.removeEventListener("keydown", downListener);
+        window.removeEventListener("keyup", upListener);
+    };
+    
+    return key;
+
+}
